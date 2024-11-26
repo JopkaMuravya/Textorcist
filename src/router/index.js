4553bfb2c1ecdb063c2f3d9e5004562cdb1ecd8e
Nania01 <148682556+Nania01@users.noreply.github.com>
2024-11-26 22:54:57 +1000

    Player Ratings

--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,40 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainMenu from '../components/MainMenu.vue';
 import GameComponent from '../components/GameComponent.vue';
 import UserRegister from '../components/UserRegister.vue';
-import UserLogin from '../components/UserLogin.vue'; 
+import UserLogin from '../components/UserLogin.vue';
+import PlayerLeaderboard from '../components/PlayerLeaderboard.vue'; 
 
 const routes = [
   {
     path: '/',
     name: 'MainMenu',
-    component: MainMenu
+    component: MainMenu,
   },
   {
     path: '/game',
     name: 'Game',
-    component: GameComponent
+    component: GameComponent,
   },
   {
     path: '/register',
     name: 'Register',
-    component: UserRegister
+    component: UserRegister,
   },
   {
     path: '/login',
     name: 'Login',
-    component: UserLogin
-  }
+    component: UserLogin,
+  },
+  {
+    path: '/leaderboard', 
+    name: 'Leaderboard',
+    component: PlayerLeaderboard,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
